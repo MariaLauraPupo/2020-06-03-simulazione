1b261da45ab6f702832c2a429f5f257376561fed
Hp <Hp@LAPTOP-N2LMCUK2.station>
2021-06-24 15:04:51 +0200

    soluzione punto a

--- src/main/java/it/polito/tdp/PremierLeague/db/PremierLeagueDAO.java
+++ src/main/java/it/polito/tdp/PremierLeague/db/PremierLeagueDAO.java
@@ -5,33 +5,36 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.PremierLeague.model.Action;
 import it.polito.tdp.PremierLeague.model.Player;
 
 public class PremierLeagueDAO {
 	
-	public List<Player> listAllPlayers(){
+	public void listAllPlayers(Map<Integer,Player> map){
 		String sql = "SELECT * FROM Players";
-		List<Player> result = new ArrayList<Player>();
+//		List<Player> result = new ArrayList<Player>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
-
+				if(!map.containsKey(res.getInt("PlayerID"))) {
 				Player player = new Player(res.getInt("PlayerID"), res.getString("Name"));
-				
-				result.add(player);
+				map.put(res.getInt("PlayerID"), player);
+				}
+//				result.add(player);
 			}
 			conn.close();
-			return result;
+//			return result;
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return null;
+//			return null;
 		}
 	}
 	
@@ -58,5 +61,32 @@ public class PremierLeagueDAO {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	public List<Player> getCalciatoreByGoal(double mediaGoal, Map<Integer,Player> map) {
+		String sql = "SELECT p.PlayerID AS pId, p.Name AS nome "
+				+ "FROM players p, actions a  "
+				+ "WHERE p.PlayerID = a.PlayerID "
+				+ "GROUP BY p.PlayerID, p.Name  "
+				+ "HAVING AVG (a.Goals) > ?";
+		List<Player> result = new ArrayList<Player>();
+		Connection conn = DBConnect.getConnection();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setDouble(1, mediaGoal);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				result.add(map.get(res.getInt("pId")));
+
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	
 	}
 }
--- src/main/java/it/polito/tdp/PremierLeague/model/Model.java
+++ src/main/java/it/polito/tdp/PremierLeague/model/Model.java
@@ -1,5 +1,27 @@
 package it.polito.tdp.PremierLeague.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.PremierLeague.db.PremierLeagueDAO;
+
 public class Model {
+	private SimpleDirectedWeightedGraph<Player, DefaultWeightedEdge> grafo;
+	private PremierLeagueDAO dao;
+	private Map<Integer,Player> idMap;
+	
+	public Model() {
+		dao = new PremierLeagueDAO();
+		idMap = new HashMap<Integer, Player>();
+		dao.listAllPlayers(idMap);
+	}
+	public void creaGrafo(double x) {
+		grafo = new SimpleDirectedWeightedGraph<Player, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		Graphs.addAllVertices(grafo, dao.getCalciatoreByGoal(x,idMap));
+	}
 
 }
